1677af4d1d070fd6fd19589158ed04e2254075f1
Alex Morais <thatsamorais@gmail.com>
2020-08-31 21:21:54 -0600

    Added work handling and notification task queuing
    * work is handled over tcp
    * unmarshaled using 'proto'
    * task_create method dropped in from google docs
    * notify queue guaranteed by defer call

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/moraisworkrunner/executor
 
 go 1.13
+
+require (
+	cloud.google.com/go v0.65.0
+	github.com/moraisworkrunner/work-messages v0.0.0-20200901020349-70f9c3a9d8ac
+	golang.org/x/net v0.0.0-20200822124328-c89045814202
+	google.golang.org/genproto v0.0.0-20200825200019-8632dd797987
+	google.golang.org/grpc v1.31.1
+	google.golang.org/protobuf v1.25.0
+)
--- main.go
+++ main.go
@@ -2,30 +2,72 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+
+	work_messages "github.com/moraisworkrunner/work-messages"
+	"google.golang.org/protobuf/proto"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	log.Print("helloworld: received a request")
-	target := os.Getenv("TARGET")
+func handler(_ http.ResponseWriter, r *http.Request) {
+	workResponse := work_messages.SvcWorkResponse{}
+
+	target := os.Getenv("NOTIFIER_QUEUE")
 	if target == "" {
-		target = "World"
+		target = "queue"
 	}
-	fmt.Fprintf(w, "Hello %s!\n", target)
+	location := os.Getenv("NOTIFIER_LOCATION")
+	if location == "" {
+		location = "nowhere"
+	}
+
+	log.Print("received a request")
+
+	// Issue a task to the notifier queue once handling has completed
+	defer func(r *work_messages.SvcWorkResponse) {
+		body, err := proto.Marshal(r)
+		if err != nil {
+
+		}
+		createTask("moraisworkrunner", location, target, string(body))
+	}(&workResponse)
+
+	// Parse the body and protobuf message from the request
+	in := &work_messages.SvcWorkRequest{}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		workResponse.Error = &work_messages.Error{
+			Message: fmt.Sprintf("Failed to read request"),
+		}
+		return
+	}
+	if err := proto.Unmarshal(body, in); err != nil {
+		workResponse.Error = &work_messages.Error{
+			Message: fmt.Sprintf("Failed to parse work request: %s", err),
+		}
+		return
+	}
+	workResponse.Context = in.Context
+
+	// process the work request
+	processWork(in)
+}
+
+func processWork(in *work_messages.SvcWorkRequest) {
+	// do "work" with the request
+	fmt.Printf("%s - %s", in.FileMetadata.GetMd5(), in.SourceFile)
+
 }
 
 func main() {
-	log.Print("helloworld: starting server...")
-
+	log.Print("starting server...")
 	http.HandleFunc("/", handler)
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-
-	log.Printf("helloworld: listening on port %s", port)
+	log.Printf("listening on port %s", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
